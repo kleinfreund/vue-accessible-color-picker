f32e0bf48b2d411dfb768a0b83a9aae4d9d67643
kleinfreund <philipprudloff@fastmail.com>
2020-12-19 19:12:52 +0100

    chore: add GitHub release assets
    Adds a configuration to the semantic-release configuration that causes a few assets (i.e. files in the dist directory) to be attached to future release entries on GitHub.

--- release.config.js
+++ release.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   branches: ['main', 'next', { name: 'beta', prerelease: true }],
   plugins: [
+    ['@semantic-release/github', {
+      assets: [
+        { path: 'dist/vue-accessible-color-picker.js' },
+        { path: 'dist/vue-accessible-color-picker-unstyled.js' },
+        { path: 'dist/index.d.ts' },
+      ],
+    }],
     '@semantic-release/git',
   ],
 }
