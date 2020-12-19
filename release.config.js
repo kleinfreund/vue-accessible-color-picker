d40f84a51342ee07e226020c5467a5835ae909c1
kleinfreund <philipprudloff@fastmail.com>
2020-12-19 21:19:54 +0100

    chore: add changelog plugin to semantic-release configuration
    Adds the `@semantic-release/changelog` plugin to the semantic-release configuration. It’ll create and update a CHANGELOG.md file for future releases.

--- release.config.js
+++ release.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   branches: ['main', 'next', { name: 'beta', prerelease: true }],
   plugins: [
+    // https://github.com/semantic-release/commit-analyzer
     '@semantic-release/commit-analyzer',
+
+    // https://github.com/semantic-release/release-notes-generator
     '@semantic-release/release-notes-generator',
+
+    // https://github.com/semantic-release/changelog
+    '@semantic-release/changelog',
+
+    // https://github.com/semantic-release/github
     ['@semantic-release/github', {
       assets: [
         { path: 'dist/vue-accessible-color-picker.js' },
@@ -10,6 +18,8 @@ module.exports = {
         { path: 'dist/index.d.ts' },
       ],
     }],
+
+    // https://github.com/semantic-release/git
     '@semantic-release/git',
   ],
 }
