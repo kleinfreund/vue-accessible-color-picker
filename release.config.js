118b103e133006747b03478b511107c400d56d9c
kleinfreund <philipprudloff@fastmail.com>
2020-12-19 19:20:37 +0100

    chore: add commit-analyzer and release-notes-generator
    Adds the commit-analyzer and release-notes-generator plugins to the semantic-release configuration in order to populate release notes for future GitHub releases.

--- release.config.js
+++ release.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   branches: ['main', 'next', { name: 'beta', prerelease: true }],
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     ['@semantic-release/github', {
       assets: [
         { path: 'dist/vue-accessible-color-picker.js' },
