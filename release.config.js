8719b9dbfa8b8799f6a7b8300be2cee2d93c8e3d
kleinfreund <philipprudloff@fastmail.com>
2020-12-20 11:03:18 +0100

    chore: add npm plugin to semantic-release configuration
    Adds the `@semantic-release/npm` plugin in order to update the project’s package.json and package-lock.json files after releasing a new version.
    
    Manually changes the package.json and package-lock.json files’ version field to 1.1.1 to match the latest release.

--- release.config.js
+++ release.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   branches: ['main', 'next', { name: 'beta', prerelease: true }],
   plugins: [
+    // This analyzes all new commits and determines whether to release a new version.
     // https://github.com/semantic-release/commit-analyzer
     '@semantic-release/commit-analyzer',
 
+    // This takes the releasing commits’ messages and compiles release notes to be used for the CHANGELOG.md file and the GitHub release.
     // https://github.com/semantic-release/release-notes-generator
     '@semantic-release/release-notes-generator',
 
+    // This creates/updates the CHANGELOG.md file.
     // https://github.com/semantic-release/changelog
     '@semantic-release/changelog',
 
+    // This updates the package.json and package-lock.json files with the new version number.
+    // https://github.com/semantic-release/npm
+    '@semantic-release/npm',
+
+    // This creates a GitHub release and attaches assets to it.
     // https://github.com/semantic-release/github
     ['@semantic-release/github', {
       assets: [
@@ -19,6 +27,7 @@ module.exports = {
       ],
     }],
 
+    // This creates a release commit in the git repository.
     // https://github.com/semantic-release/git
     '@semantic-release/git',
   ],
