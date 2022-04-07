f7e4de296ecf3234a6aa79ab78c150f880074e3d
kleinfreund <philipprudloff@fastmail.com>
2022-04-07 19:06:51 +0200

    chore(types): provide types for importing Vue components
    Adds a type definition file for .vue files which ensures that importing a Vue single file component doesn’t trigger a TypeScript error.

---
+++ types/vue-shims.d.ts
@@ -0,0 +1,10 @@
+// Fixes the following TypeScript error when importing a Vue single file component:
+// Cannot find module './ColorPicker.vue' or its corresponding type declarations.
+
+declare module '*.vue' {
+  import { defineComponent } from 'vue'
+
+  const component: ReturnType<typeof defineComponent>
+
+  export default component
+}
