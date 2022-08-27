3cca933fd3ed1a44533d4c3e018f4669b7794ade
kleinfreund <philipprudloff@fastmail.com>
2022-08-27 21:51:27 +0200

    chore: simplifies round function

--- src/utilities/round.test.js
+++ src/utilities/round.test.js
@@ -10,6 +10,7 @@ describe('round', () => {
     [10.333, '10.33'],
     [10.333333333, '10.33'],
     [10.00333333333, '10'],
+    [10, '10'],
   ]))('works (one argument)', (value, expectedValue) => {
     expect(round(value)).toBe(expectedValue)
   })
@@ -20,6 +21,7 @@ describe('round', () => {
     [10.333, 4, '10.333'],
     [10.333333333, 4, '10.3333'],
     [10.00333333333, 4, '10.0033'],
+    [10, 4, '10'],
   ]))('works (two arguments)', (value, precision, expectedValue) => {
     expect(round(value, precision)).toBe(expectedValue)
   })
