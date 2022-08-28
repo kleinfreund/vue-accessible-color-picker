a685d152c1e21cdc530383c42403e1c69e138c6e
kleinfreund <philipprudloff@fastmail.com>
2022-08-28 16:05:03 +0200

    chore: removes unnecessary ESLint options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
 /** @type {import('eslint').Linter.Config} */ const config = {
+  plugins: ['vue'],
+  extends: ['standard', 'plugin:vue/vue3-recommended'],
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'space-before-function-paren': ['error', 'always'],
   },
-  globals: {
-    defineEmits: 'readonly',
-    defineProps: 'readonly',
-  },
-  plugins: ['vue'],
-  extends: ['standard', 'plugin:vue/vue3-recommended'],
 }
 
 module.exports = config
