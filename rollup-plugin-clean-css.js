035e2d8f01df77a402f35f5ef14207152247e152
kleinfreund <philipprudloff@fastmail.com>
2022-09-04 14:29:44 +0200

    refactor: improves cleanCss plugin

--- rollup-plugin-clean-css.js
+++ rollup-plugin-clean-css.js
@@ -1,16 +1,31 @@
+import { createFilter } from '@rollup/pluginutils'
 import CleanCSS from 'clean-css'
 
 /** @typedef {import('rollup').Plugin} Plugin */
+/** @typedef {import('rollup').TransformResult} TransformResult */
+/** @typedef {import('@rollup/pluginutils').FilterPattern} FilterPattern */
+/**
+ * @typedef {object} CleanCssPluginOptions
+ * @property {FilterPattern} [include] Determines with modules to include by their ID.
+ * @property {FilterPattern} [exclude] Determines with modules to exclude by their ID.
+ * @property {CleanCSS.OptionsOutput} [cleanCssOptions] Options to pass along to `new CleanCSS`.
+ */
 
 /**
  * Minifies CSS using [CleanCSS][1]. Passes CleanCSS options along if provided.
+ *
+ * Does not bundle CSS.
  *
  * **Example usage**:
  *
  * ```js
  * plugins: [
  *   vue(),
- *   cleanCss({ cleanCssOptions: { sourceMap: true } }),
+ *   cleanCss({
+ *     cleanCssOptions: {
+ *       sourceMap: true,
+ *     },
+ *   }),
  *   postcss(),
  *   terser(),
  * ],
@@ -18,34 +33,49 @@ import CleanCSS from 'clean-css'
  *
  * [1]: https://www.npmjs.com/package/clean-css
  *
- * @param {{ cleanCssOptions?: CleanCSS.OptionsOutput }} options
+ * @param {CleanCssPluginOptions} [pluginOptions]
  * @returns {Plugin}
  */
-export function cleanCss (options = {}) {
+export function cleanCss (pluginOptions = {}) {
+  const filter = createFilter(pluginOptions.include ?? '**/*.css', pluginOptions.exclude)
+
   return {
     name: 'clean-css',
+
     transform (code, id) {
-      // Only processes CSS files.
-      if (!id.endsWith('.css')) {
-        return null
+      if (!filter(id)) {
+        return
       }
 
-      const { styles, errors, warnings, sourceMap } = new CleanCSS(options.cleanCssOptions).minify(code)
+      if (!pluginOptions.cleanCssOptions) {
+        pluginOptions.cleanCssOptions = {}
+      }
+
+      // Enables source map generation by default.
+      // See https://rollupjs.org/guide/en/#source-code-transformations.
+      if (!('sourceMap' in pluginOptions.cleanCssOptions)) {
+        pluginOptions.cleanCssOptions.sourceMap = true
+      }
+
+      const { styles, errors, warnings, sourceMap } = new CleanCSS(pluginOptions.cleanCssOptions).minify(code)
 
       for (const warning of warnings) {
         this.warn(warning)
       }
 
       if (errors.length > 0) {
-        // Concatenates all errors because calling `this.error` will throw and so looping like it’s done for warnings isn’t feasible.
+        // Concatenates all errors first since calling `this.error` will throw and so looping like it’s done for warnings would only report one error at a time.
         this.error(errors.join('\n'))
       }
 
-      const transformResult = { code: styles }
+      /** @type {TransformResult} */ const transformResult = { code: styles }
 
-      // Note: This is necessary. The current type of sourceMap (`SourceMapGenerator`) is not accurate as the `sourceMap` property isn’t even returned by CleanCSS when not setting `sourceMap: true`.
-      if (sourceMap !== undefined) {
-        transformResult.map = sourceMap.toString()
+      // The current type of `Output.sourceMap` is incomplete. The `sourceMap` property should be optional as it is only included in `Output` if the CleanCSS `sourceMap` option is `true`.
+      // The root cause is an incorrect type definition in https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/clean-css/index.d.ts#L97.
+      const sourceMapGenerator = /** @type {typeof sourceMap | undefined} */ (sourceMap)
+
+      if (sourceMapGenerator !== undefined) {
+        transformResult.map = sourceMapGenerator.toString()
       }
 
       return transformResult
