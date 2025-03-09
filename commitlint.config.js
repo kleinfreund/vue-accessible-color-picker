e771139086911e2300d2f8872a7e04c163d10281
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-03-09 11:27:48 +0100

    chore(deps-dev): downgrade vite/vue-tsc due to 'Unable to follow symbol for "__VLS_PrettifyGlobal"' error

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@commitlint/types').UserConfig} */ const config = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
+		'header-max-length': [0, 'always', Infinity],
 		'body-max-line-length': [0, 'always', Infinity],
 		'footer-max-line-length': [0, 'always', Infinity],
 	},
